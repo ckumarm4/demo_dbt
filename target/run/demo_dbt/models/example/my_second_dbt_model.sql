bdaeb2e3ba9b92b7981bf0b465af0d5b2321cbdd
Chithra Kumar <chithrakumar@Chithras-MacBook-Pro.local>
2022-01-03 14:51:06 -0800

    added code

---
+++ target/run/demo_dbt/models/example/my_second_dbt_model.sql
@@ -0,0 +1,10 @@
+
+  create or replace  view edw_db_dev.adhoc_schema.my_second_dbt_model 
+  
+   as (
+    -- Use the `ref` function to select from other models
+
+select *
+from edw_db_dev.adhoc_schema.my_first_dbt_model
+where id = 1
+  );
